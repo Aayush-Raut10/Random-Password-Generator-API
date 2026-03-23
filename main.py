f76a1ce2f63176ccaac25c1e4d0a9315e1fb7b9b
Aayush-Raut10 <aayushraut209@gmail.com>
2026-03-23 21:56:41 +0545

    created strong password checking endpoint

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from fastapi import FastAPI
 import string
 import random
-
+SPECIAL_SYMBOLS = string.punctuation
 
 
 app = FastAPI()
 
 
-@app.get("/password")
+@app.get("/generate_password")
 def generate_password(length:int = 8):
 
     if length <= 0:
         return {"message":"Password length can't be negative or zero !"}
     
-    if length > 20:
-        return {"message":"Password length can't be more than 30 !"}
+    if length > 500:
+        return {"message":"Password length can't be more than 500 !"}
     
     alphabets = string.ascii_letters
     numbers = string.digits
-    extra = "!@#$%^&*" * 2
+    extra = SPECIAL_SYMBOLS
 
     combination = list(alphabets + numbers + extra)
     random.shuffle(combination)
@@ -32,3 +32,33 @@ def generate_password(length:int = 8):
    
     password = "".join(password)
     return {"password":password}
+
+
+@app.post("/check_password")
+def is_password_strong(payload:dict):
+    password = payload.get("password").strip()
+    
+    if not password:
+        return {"status": "password is not provided"}
+    
+    issues = []
+    
+    if len(password) < 8:
+        issues.append("Password is less than 8 characters")
+
+    if not (any(char.isdigit() for char in password)):
+        issues.append("Missing digits")
+
+    if not (any(char.islower() for char in password)):
+        issues.append("Missing lowercase")
+    
+    if not any(c.isupper() for c in password):
+        issues.append("Missing uppercase")
+
+    if not any(char in SPECIAL_SYMBOLS for char in password):
+        issues.append("Missing special symbols")
+
+    if issues:
+        return {"is_strongest": False, "issues":issues}
+    
+    return {"is_strongest": True}
